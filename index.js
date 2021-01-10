4a240052f04907587bccc850566831ecf1dd7554
Your Name <you@example.com>
2021-01-10 12:00:35 -0500

    commit tree b0db85eef49c5b65f07f20f82c9d3037326743dc

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+// multiply
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+// random
+let random = Math.random() // *1
+
+//mod
+let mod = 28 % 12
+
+//max
+let max = Math.max(20, 15, 14, 9, 16, 4)
